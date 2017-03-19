cc21cb5af89a75dbda0cc413558f50f553f68193
ae <erradi@live.com>
2017-03-19 19:31:02 +0300

    Updated MVC Slides and Examples

--- Examples/06.MVC-JavaScript-WebApp/routes.js
+++ Examples/06.MVC-JavaScript-WebApp/routes.js
@@ -10,33 +10,34 @@ router.get('/api/students', (req, res) => studentController.getStudents (req, re
 router.get('/api/students/:id', (req, res) => studentController.getStudent(req, res) )
 
 //Heroes Web API
-router.get('/api/heroes', (req, res) => heroController.getHeroes(req, res) )
-router.get('/api/heroes/:id', (req, res) => heroController.getHero(req, res) )
-router.post('/api/heroes/', (req, res) => heroController.addHero(req, res) )
-router.put('/api/heroes/:id', (req, res) => heroController.updateHero(req, res) )
-router.delete('/api/heroes/:id', (req, res) => heroController.deleteHero(req, res) )
+router.route('/api/heroes')
+    .get( (req, res) => heroController.getHeroes(req, res) )
+    .post( (req, res) => heroController.addHero(req, res) )
+
+router.route('/api/heroes/:id')
+    .get( (req, res) => heroController.getHero(req, res) )
+    .put( (req, res) => heroController.updateHero(req, res) )
+    .delete( (req, res) => heroController.deleteHero(req, res) )
 
 //Routes returning views
-router.get('/login', (req, res) => res.sendFile(__dirname + "/views/login.html") )
-
 router.get('/', (req, res) => res.render('index') )
 
-router.post('/login', (req, res) => {
-    let userInfo = req.body
-    console.log("router.post.req.body", userInfo)
+router.route('/login')
+    .get( (req, res) => res.sendFile(__dirname + "/views/login.html") )
+    .post( (req, res) => {
+        let userInfo = req.body
+        console.log("router.post.req.body", userInfo)
 
-    //Return an accessCount cookie to the client -- expires is optional
-    res.cookie('username', userInfo.username, { expires: new Date(Date.now() + 1000 * 360 * 1) })
-    res.redirect('/')
-})
+        //Return an accessCount cookie to the client -- expires is optional
+        res.cookie('username', userInfo.username, { expires: new Date(Date.now() + 1000 * 360 * 1) })
+        res.redirect('/')
+    })
 
 router.get('/logout', (req, res) => {
     //clear cookie
     res.cookie('username', '', {expires: new Date(0)})
     res.redirect("/")
 })
-
-
 
 //Middleware to intercept requests and redirect to the login page if the user is not logged-in
 router.use( (req, res, next) => {
@@ -53,20 +54,21 @@ router.use( (req, res, next) => {
     }
 })
 
-//Middleware to intercept requests and redirect to the login page if the user is not logged-in. Only apllies to /students and /heroes
-/*function isAuthenticated(req, res, next) {
-    const username = req.cookies.username
-    console.log("isAuthenticated.username", username)
-    if (!username) {
-        res.redirect("/login")
-    } else {
-        return next()
-    }
-}*/
-
 router.get('/students', (req, res) => studentController.index(req, res) )
 
-router.get('/heroes', (req, res) => heroController.index(req, res) )
-router.post('/heroes', (req, res) => heroController.postHero(req, res) )
+router.route('/heroes')
+    .get( (req, res) => heroController.index(req, res) )
+    .post( (req, res) => heroController.postHero(req, res) )
 
 module.exports = router
+
+//Middleware to intercept requests and redirect to the login page if the user is not logged-in. Only apllies to /students and /heroes
+/*function isAuthenticated(req, res, next) {
+     const username = req.cookies.username
+     console.log("isAuthenticated.username", username)
+     if (!username) {
+        res.redirect("/login")
+     } else {
+        return next()
+     }
+ }*/
