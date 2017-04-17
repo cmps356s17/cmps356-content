54e8bf4fac0d7f74f7577334dc2dcc4b40b62f7f
Abdulahi Hassen <abdala.baaji@gmail.com>
2017-04-17 06:55:26 +0300

    local file server commit message

--- Examples/06.MVC-JavaScript-WebApp/app.js
+++ Examples/06.MVC-JavaScript-WebApp/app.js
@@ -1,22 +1,21 @@
-let express		 =	require('express')
-let bodyParser   = 	require('body-parser')
-let cookieParser = 	require('cookie-parser')
-let handlebars   =  require('express-handlebars')
-
-let app			 =	express()
+let express = require('express')
+let bodyParser = require('body-parser')
+let cookieParser = require('cookie-parser')
+let handlebars = require('express-handlebars')
+let app = express()
 
 //Allow serving static files from __dirname which is the current folder
-app.use( express.static(__dirname) )
+app.use(express.static(__dirname))
 
 /*
  body-parser extracts the entire body portion of an incoming request and assigns it to req.body.
  Parses the body text as URL encoded data (which is how browsers send form data from forms with method set to POST)
  and exposes the resulting object (containing the keys and values) on req.body.
  */
-app.use( bodyParser.urlencoded({extended: true}) )
+app.use(bodyParser.urlencoded({extended: true}))
 //If the body of incoming request is a json object then assign it to req.body property
-app.use( bodyParser.json() )
-app.use( cookieParser() )
+app.use(bodyParser.json())
+app.use(cookieParser())
 
 // Bind Handlebars View Engine to html extension so express knows what extension to look for.
 //set extension to .html so handlebars knows what to look for
