fb029b281d0630e82e8c604e535a572f0c7b7d0f
ae <erradi@live.com>
2017-05-16 18:02:14 +0300

    Test service example using expect

--- Examples/10.mongodb/test/BookService.spec.js
+++ Examples/10.mongodb/test/BookService.spec.js
@@ -9,7 +9,7 @@ describe('Book Service Test Suite', () => {
     //More info @ http://chaijs.com/plugins/chai-http/
     it('it should return an array of 5 elements', async () => {
         const response = await chai.request('http://localhost:9090').get('/api/books')
-        expect(response).to.have.property('status', 200)
+        expect(response).to.have.status(200)
         expect(response).to.have.property('body').and.be.a('array')
         expect(response).to.have.deep.property('body.length', 5)
     })
